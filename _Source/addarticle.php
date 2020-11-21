9f83942dcec0688ae29ab4dec30ab6055021601f
Pavel Fučík <fucik10@student.vspj.cz>
2020-11-21 13:51:10 +0100

    Přidána stránka na vkládání článků

--- _Source/addarticle.php
+++ _Source/addarticle.php
@@ -70,7 +70,7 @@ if (isset($_GET['logout']) && ($_GET['logout'] == 'true')) {
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="addarticle.php" class="nav-link">
+                <a href="pages/layout/top-nav.html" class="nav-link">
                   <i class="far fa-plus nav-icon"></i>
                   <p>Přidat článek</p>
                 </a>
@@ -153,171 +153,72 @@ if (isset($_GET['logout']) && ($_GET['logout'] == 'true')) {
 <!-- /.sidebar -->
 </aside>
 
+
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
-  <div class="content-header">
+  <section class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0 text-dark">Dashboard</h1>
-        </div><!-- /.col -->
+          <h1>Přidat článek</h1>
+        </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Logos Polytechnikos</a></li>
-            <li class="breadcrumb-item active">Homepage</li>
+            <li class="breadcrumb-item"><a href="#">Domů</a></li>
+            <li class="breadcrumb-item active">Přidat článek</li>
           </ol>
-        </div><!-- /.col -->
-      </div><!-- /.row -->
+        </div>
+      </div>
     </div><!-- /.container-fluid -->
-  </div>
-  <!-- /.content-header -->
+  </section>
 
   <!-- Main content -->
   <section class="content">
-    <div class="container-fluid">
-      <!-- Small boxes (Stat box) -->
-      <div class="row">
-        <div class="col-lg-4 col-6">
-          <!-- small box -->
-          <div class="small-box bg-info">
-            <div class="inner">
-              <h3>150</h3>
-
-              <p>Nových článků ke schválení</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
-            </div>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-4 col-6">
-          <!-- small box -->
-          <div class="small-box bg-warning">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>Zaregistrovaných uživatelů</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-4 col-6">
-          <!-- small box -->
-          <div class="small-box bg-danger">
-            <div class="inner">
-              <h3>65</h3>
-
-              <p>Návštěvníků dnes</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-          </div>
-        </div>
-        <!-- ./col -->
-      </div>
-      <!-- /.row -->
-      <!-- Main row -->
-      <div class="row">
-        <!-- Left col -->
-        <section class="col-lg-7 connectedSortable">
-         <!-- Calendar -->
-         <div class="card bg-gradient-success">
-          <div class="card-header border-0">
-
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card card-outline card-info">
+          <div class="card-header">
             <h3 class="card-title">
-              <i class="far fa-calendar-alt"></i>
-              Kalendář
+              Níže vložte svůj článek
             </h3>
+            <!-- tools box -->
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool btn-sm" data-card-widget="collapse" data-toggle="tooltip"
+                      title="Collapse">
+                <i class="fas fa-minus"></i></button>
+              <button type="button" class="btn btn-tool btn-sm" data-card-widget="remove" data-toggle="tooltip"
+                      title="Remove">
+                <i class="fas fa-times"></i></button>
+            </div>
+            <!-- /. tools -->
           </div>
           <!-- /.card-header -->
-          <div class="card-body pt-0">
-            <!--The calendar -->
-            <div id="calendar" style="width: 100%"></div>
+          <div class="card-body pad">
+            <div class="mb-3">
+              <textarea class="textarea" placeholder="Place some text here"
+                        style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+            </div>
+            <p class="text-sm mb-0">
+              Editor <a href="https://github.com/summernote/summernote">Documentation and license
+              information.</a>
+            </p>
           </div>
-          <!-- /.card-body -->
         </div>
-
-
-
-
-
-      </section>
-      <!-- /.Left col -->
-      <!-- right col (We are only adding the ID to make the widgets sortable)-->
-      <section class="col-lg-5 connectedSortable">
-
-        <!-- Map card -->
-        <div class="card bg-gradient-primary">
-          <div class="card-header border-0">
-            <h3 class="card-title">
-              <i class="fas fa-map-marker-alt mr-1"></i>
-              Návštěvníci
-            </h3>
-            <!-- card tools -->
-            <div class="card-tools">
-              <button type="button"
-              class="btn btn-primary btn-sm daterange"
-              data-toggle="tooltip"
-              title="Date range">
-              <i class="far fa-calendar-alt"></i>
-            </button>
-            <button type="button"
-            class="btn btn-primary btn-sm"
-            data-card-widget="collapse"
-            data-toggle="tooltip"
-            title="Collapse">
-            <i class="fas fa-minus"></i>
-          </button>
-        </div>
-        <!-- /.card-tools -->
       </div>
-      <div class="card-body">
-        <div id="world-map" style="height: 250px; width: 100%;"></div>
-      </div>
-      <!-- /.card-body-->
-      <div class="card-footer bg-transparent">
-        <div class="row">
-          <div class="col-4 text-center">
-            <div id="sparkline-1"></div>
-            <div class="text-white">Dnes</div>
-          </div>
-          <!-- ./col -->
-          <div class="col-4 text-center">
-            <div id="sparkline-2"></div>
-            <div class="text-white">Včera</div>
-          </div>
-          <!-- ./col -->
-          <div class="col-4 text-center">
-            <div id="sparkline-3"></div>
-            <div class="text-white">Předevčírem</div>
-          </div>
-          <!-- ./col -->
-        </div>
-        <!-- /.row -->
-      </div>
+      <!-- /.col-->
     </div>
-    <!-- /.card -->
-
-
-
+    <!-- ./row -->
   </section>
-  <!-- right col -->
-</div>
-<!-- /.row (main row) -->
-</div><!-- /.container-fluid -->
-</section>
-<!-- /.content -->
+  <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-  <strong>Copyright &copy; 2020 DeathNote</strong> . Běží na šabloně: <a href="http://adminlte.io">AdminLTE.io</a>.
-  All rights reserved.
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 3.0.5
+  </div>
+  <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
+  reserved.
 </footer>
 
 <!-- Control Sidebar -->
