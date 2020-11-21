9fc59ba774e10f2370e4e55d40773f1cb3699de6
Pavel Fučík <fucik10@student.vspj.cz>
2020-11-21 15:26:42 +0100

    Přidat článek v1.2

--- _Source/addarticle.php
+++ _Source/addarticle.php
@@ -186,9 +186,10 @@ if (isset($_GET['logout']) && ($_GET['logout'] == 'true')) {
           </div>
           <!-- /.card-header -->
           <div class="card-body pad">
+            <h3 class="card-title"><b>Zadejte obsah článku:</b></h3></br>
             <div class="mb-3">
               <textarea class="textarea" placeholder="Place some text here"
-                        style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                        style="width: 100%; height: 300px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></br></br></br></br></br></br></br></br></br></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Odeslat</button> 
           </div>
