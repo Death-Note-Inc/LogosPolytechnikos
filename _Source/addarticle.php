f488475cce08c1375be6cd28e6722babe82ee74f
Pavel Fučík <fucik10@student.vspj.cz>
2020-11-21 14:48:24 +0100

    Přidat článek v1.1

--- _Source/addarticle.php
+++ _Source/addarticle.php
@@ -179,18 +179,9 @@ if (isset($_GET['logout']) && ($_GET['logout'] == 'true')) {
       <div class="col-md-12">
         <div class="card card-outline card-info">
           <div class="card-header">
-            <h3 class="card-title">
-              Níže vložte svůj článek
-            </h3>
-            <!-- tools box -->
-            <div class="card-tools">
-              <button type="button" class="btn btn-tool btn-sm" data-card-widget="collapse" data-toggle="tooltip"
-                      title="Collapse">
-                <i class="fas fa-minus"></i></button>
-              <button type="button" class="btn btn-tool btn-sm" data-card-widget="remove" data-toggle="tooltip"
-                      title="Remove">
-                <i class="fas fa-times"></i></button>
-            </div>
+            <h3 class="card-title"><b>Zadejte název článku:</b></h3>
+            </br>
+            <input type="text" class="form-control" placeholder="Název...">
             <!-- /. tools -->
           </div>
           <!-- /.card-header -->
@@ -199,10 +190,7 @@ if (isset($_GET['logout']) && ($_GET['logout'] == 'true')) {
               <textarea class="textarea" placeholder="Place some text here"
                         style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
             </div>
-            <p class="text-sm mb-0">
-              Editor <a href="https://github.com/summernote/summernote">Documentation and license
-              information.</a>
-            </p>
+            <button type="submit" class="btn btn-primary">Odeslat</button> 
           </div>
         </div>
       </div>
