fa7181551d09ef432f9ebbb71cb4cfd21e076f6a
Pavel Fučík <fucik10@student.vspj.cz>
2020-11-24 21:57:06 +0100

    login + index change

--- _Source/index.php
+++ _Source/index.php
@@ -6,89 +6,196 @@ error_reporting(E_ALL);
 //load dependeces
 include_once("parts/db-connect.php");
 
-
-
-//redurect user if logged, disable on debug
-/*if ($user->is_logged_in()) {
-  $user->redirect('home.php');
+//redirtect user if not logged in
+if (!$user->is_logged_in()) {
+    $user->redirect('index.php');
 }
-*/
 
+//check the logout button
+if (isset($_GET['logout']) && ($_GET['logout'] == 'true')) {
+    $user->log_out();
+    $user->redirect('index.php');
+    
+}
 
-//check if login form submitted
-if (isset($_POST['log_in'])) {
-  echo "login";
- $user_email = trim($_POST['user_name_email']);
- $user_password = trim($_POST['user_password']);
- if ($user->login($user_email, $user_password)) {
-    $user->redirect('home.php');
-    } else $chyba = true;
-  }
 ?>
 
 
 <!DOCTYPE html>
 <html>
 <?php include_once("parts/header.php");?>
-<body class="hold-transition login-page">
-  <div class="login-box">
-    <div class="login-logo">
-      <a href="../../index2.html"><b>Logos</b>Polytechnikos</a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="card">
-      <div class="card-body login-card-body">
-        <p class="login-box-msg">Přihlaste se prosím</p>
-        <?php 
-        if (isset($chyba)) {
-          echo "Přihlašování se nezdařilo";
-        }
-        ?>
 
-        <form action="index.php" method="post">
-          <div class="input-group mb-3">
-            <input type="email" id="user_name_email" name="user_name_email" class="form-control" placeholder="Email">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" id="user_password" name="user_password" class="form-control" placeholder="Heslo">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <!-- /.col -->
-            <div class="col-12">
-              <button type="submit" name="log_in" class="btn btn-primary btn-block">Přihlásit se</button>
-            </div>
-            <!-- /.col -->
-          </div>
-        </form>
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+    <?php include_once("parts/nav.php");?>
+</div>
 
 
-
-        <p class="mb-1">
-          <a href="forgot-password.html">Zapomenuté heslo</a>
-        </p>
-      </div>
-      <!-- /.login-card-body -->
-    </div>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0 text-dark">Dashboard</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Logos Polytechnikos</a></li>
+            <li class="breadcrumb-item active">Homepage</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
   </div>
-  <!-- /.login-box -->
+  <!-- /.content-header -->
 
-  <!-- jQuery -->
-  <script src="../../plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="../../dist/js/adminlte.min.js"></script>
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-4 col-6">
+          <!-- small box -->
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3><?php echo $post->getWaitingCount() ?></h3>
+
+              <p>Nových článků ke schválení</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-6">
+          <!-- small box -->
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h3><?php echo $user->getUsersCount() ?></h3>
+
+              <p>Zaregistrovaných uživatelů</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-6">
+          <!-- small box -->
+          <div class="small-box bg-danger">
+            <div class="inner">
+              <h3><?php echo $post->getTotalCount() ?></h3>
+
+              <p>Celkem článků</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+      <!-- Main row -->
+      <div class="row">
+        <!-- Left col -->
+        <section class="col-lg-7 connectedSortable">
+         <!-- Calendar -->
+         <div class="card bg-gradient-success">
+          <div class="card-header border-0">
+
+            <h3 class="card-title">
+              <i class="far fa-calendar-alt"></i>
+              Kalendář
+            </h3>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body pt-0">
+            <!--The calendar -->
+            <div id="calendar" style="width: 100%"></div>
+          </div>
+          <!-- /.card-body -->
+        </div>
+
+
+
+
+
+      </section>
+      <!-- /.Left col -->
+      <!-- right col (We are only adding the ID to make the widgets sortable)-->
+      <section class="col-lg-5 connectedSortable">
+
+        <!-- Map card -->
+        <div class="card bg-gradient-primary">
+          <div class="card-header border-0">
+            <h3 class="card-title">
+              <i class="fas fa-map-marker-alt mr-1"></i>
+              Návštěvníci
+            </h3>
+            <!-- card tools -->
+            <div class="card-tools">
+              <button type="button"
+              class="btn btn-primary btn-sm daterange"
+              data-toggle="tooltip"
+              title="Date range">
+              <i class="far fa-calendar-alt"></i>
+            </button>
+            <button type="button"
+            class="btn btn-primary btn-sm"
+            data-card-widget="collapse"
+            data-toggle="tooltip"
+            title="Collapse">
+            <i class="fas fa-minus"></i>
+          </button>
+        </div>
+        <!-- /.card-tools -->
+      </div>
+      <div class="card-body">
+        <div id="world-map" style="height: 250px; width: 100%;"></div>
+      </div>
+      <!-- /.card-body-->
+      <div class="card-footer bg-transparent">
+        <div class="row">
+          <div class="col-4 text-center">
+            <div id="sparkline-1"></div>
+            <div class="text-white">Dnes</div>
+          </div>
+          <!-- ./col -->
+          <div class="col-4 text-center">
+            <div id="sparkline-2"></div>
+            <div class="text-white">Včera</div>
+          </div>
+          <!-- ./col -->
+          <div class="col-4 text-center">
+            <div id="sparkline-3"></div>
+            <div class="text-white">Předevčírem</div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <!-- /.row -->
+      </div>
+    </div>
+    <!-- /.card -->
+
+
+
+  </section>
+  <!-- right col -->
+</div>
+<!-- /.row (main row) -->
+</div><!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+</div>
+
+
+ <?php include_once("parts/footer.php");?>
+
 
 </body>
 </html>
-
