48a745ccf35a534acd37addb2df54a1628b3f5cc
Pavel Fučík <fucik10@student.vspj.cz>
2020-11-24 21:54:55 +0100

    Public page v1.0

---
+++ _Source/main.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>LOGOS POLYTECHNIKOS</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <!-- Custom fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles -->
+  <link href="css/landing-page.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-light bg-light static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">LOGOS POLYTECHNIKOS</a>
+      <a class="btn btn-primary" href="index.php">Přihlásit se</a>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class="mb-5">LOGOS POLYTECHNIKOS je vysokoškolský odborný recenzovaný časopis...</h1>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+          <form>
+            <div class="form-row">
+              <div class="col-12 col-md-8 mb-2 mb-md-0">
+                <input type="email" class="form-control form-control-lg" placeholder="Vložte Váš E-mail...">
+              </div>
+              <div class="col-12 col-md-4">
+                <button type="submit" class="btn btn-block btn-lg btn-primary">Odebírat vydání!</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Icons Grid -->
+  <section class="features-icons bg-light text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="col-xs-1" align="center">
+              <img src="img/vydani.png" alt="Girl in a jacket">
+            </div>
+            <h3>Mrtví na hladině</h3>
+            <p>Blbaba blabla blabla</p>
+            <button type="button" class="btn btn-primary">Stáhnout</button>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="col-xs-1" align="center">
+              <img src="img/vydani2.png" alt="Girl in a jacket">
+            </div>
+            <h3>Lidské cesty</h3>
+            <p>Tedaju juearj jujuja</p>
+            <button type="button" class="btn btn-primary">Stáhnout</button>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="col-xs-1" align="center">
+              <img src="img/vydani.png" alt="Girl in a jacket">
+            </div>
+            <h3>Mrtví na hladině</h3>
+            <p>Eararfk okfeaokf okfaokf</p>
+            <button type="button" class="btn btn-primary">Stáhnout</button>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="col-xs-1" align="center">
+              <img src="img/vydani2.png" alt="Girl in a jacket">
+            </div>
+            <h3>Mrtví na hladině</h3>
+            <p>Pleald dlaodl oopdad</p>
+            <button type="button" class="btn btn-primary">Stáhnout</button>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="col-xs-1" align="center">
+              <img src="img/vydani.png" alt="Girl in a jacket">
+            </div>
+            <h3>Mrtví na hladině</h3>
+            <p>Apdepad apdaed adeaaaad</p>
+            <button type="button" class="btn btn-primary">Stáhnout</button>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+              <div class="col-xs-1" align="center">
+                <img src="img/vydani2.png" alt="Girl in a jacket">
+              </div>
+              <h3>Mrtví na hladině</h3>
+              <p>Adawdůoe dladkad dadead</p>
+              <button type="button" class="btn btn-primary">Stáhnout</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action -->
+  <section class="call-to-action text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h2 class="mb-4">Jste připraveni odebírat náš časopis?</h2>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+          <form>
+            <div class="form-row">
+              <div class="col-12 col-md-8 mb-2 mb-md-0">
+                <input type="email" class="form-control form-control-lg" placeholder="Vložte Váš E-mail...">
+              </div>
+              <div class="col-12 col-md-4">
+                <button type="submit" class="btn btn-block btn-lg btn-primary">Odebírat vydání!</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="footer bg-light">
+    <div class="container">
+        <div class="text-center my-auto">
+          <p>&copy; LOGOS POLYTECHNIKOS 2020. Created by Death Note.</p>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
